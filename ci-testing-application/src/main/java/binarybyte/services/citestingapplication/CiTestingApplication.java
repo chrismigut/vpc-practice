dd3e0b7d408b0b64fa9e1b860259ead2c8e77687
Chris Migut <cmiguthere@yahoo.com>
2018-05-20 23:28:14 -0400

    add spring boot application to try out ci testing - init commit

---
+++ ci-testing-application/src/main/java/binarybyte/services/citestingapplication/CiTestingApplication.java
@@ -0,0 +1,12 @@
+package binarybyte.services.citestingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiTestingApplication.class, args);
+	}
+}
---
+++ ci-testing-application/src/test/java/binarybyte/services/citestingapplication/CiTestingApplicationTests.java
@@ -0,0 +1,16 @@
+package binarybyte.services.citestingapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CiTestingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
